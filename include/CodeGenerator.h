b48c744a135521c4f9ddfb5377bbabaa481983ec
Razor <daniel@xeno.solutions>
2016-07-02 03:24:04 +0100

    Made the code generator do more things.

--- include/CodeGenerator.h
+++ include/CodeGenerator.h
@@ -26,17 +26,14 @@
 #define CODEGENERATOR_H
 #include <vector>
 #include <string>
+#include <map>
 #include "Tree.h"
 #include "Stream.h"
-#include <Branch.h>
+#include "Branch.h"
 #include "CodeGeneratorException.h"
+#include "GoblinObject.h"
 #include "CompilerEntity.h"
 
-struct location
-{
-    int location;
-    std::shared_ptr<Branch> branch;
-};
 
 struct scope_variable
 {
@@ -46,28 +43,65 @@ struct scope_variable
     int index;
 };
 
-class CodeGenerator : public CompilerEntity {
+struct function
+{
+    std::string func_name;
+    std::vector<struct function_argument> func_arguments;
+    int memory_pos;
+};
+
+struct function_argument
+{
+    std::string type;
+    std::string value;
+};
+
+struct function_call
+{
+    std::string func_name;
+    std::vector<struct function_argument> func_arguments;
+    int memory_pos;
+};
+
+class CodeGenerator : public CompilerEntity
+{
 public:
-    CodeGenerator(Compiler* compiler);
+    CodeGenerator(Compiler* compiler, std::string code_gen_desc);
     virtual ~CodeGenerator();
-    
     Stream* getStream();
-    void startRegistrationProcess();
-    void registerMemoryLocation(std::shared_ptr<Branch> branch);
+
+    void registerFunction(std::string func_name, std::vector<std::shared_ptr<Branch>> func_arguments, int func_mem_pos);
+    void registerFunctionCall(std::string func_name, std::vector<std::shared_ptr < Branch>> func_arguments);
     bool isFunctionRegistered(std::string func_name);
-    int getFunctionMemoryLocation(std::string func_name);
+    int getFunctionIndex(std::string func_name);
     void registerScopeVariable(std::shared_ptr<Branch> branch);
     std::shared_ptr<struct scope_variable> getScopeVariable(std::string name);
     int getScopeVariablesSize();
     void clearScopeVariables();
+    
+    GoblinObject* getGoblinObject();
+    std::string getCodeGeneratorDescriptor();
+    
     virtual void generate(std::shared_ptr<Tree> tree);
-    virtual void generateFromBranch(std::shared_ptr<Branch> branch) = 0;
+    virtual void generateFromBranch(std::shared_ptr<Branch> branch);
+    virtual void handleScope(std::shared_ptr<Branch> branch);
+
+    virtual void scope_start(std::shared_ptr<Branch> branch) = 0;
+    virtual void scope_assign_start(std::shared_ptr<Branch> branch, std::shared_ptr<struct scope_variable>) = 0;
+    virtual void scope_assign_end(std::shared_ptr<Branch> branch, std::shared_ptr<struct scope_variable>) = 0;
+    virtual void scope_func_call(std::shared_ptr<Branch> branch, std::string func_name, std::vector<std::shared_ptr < Branch>> func_arguments) = 0;
+    virtual void scope_end(std::shared_ptr<Branch> branch) = 0;
+    virtual void scope_handle_exp(std::shared_ptr<Branch> branch) = 0;
+    virtual void scope_handle_number(std::shared_ptr<Branch> branch) = 0;
 protected:
     Stream* stream;
 private:
+    std::string code_gen_desc;
     int current_index;
-    std::vector<location> locations;
+    std::vector<struct function> functions;
+    std::vector<struct function_call> function_calls;
     std::vector<std::shared_ptr<struct scope_variable>> scope_variables;
+    GoblinObject gob_obj;
 };
 
 #endif /* CODEGENERATOR_H */
--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -28,13 +28,17 @@
  */
 
 #include "CodeGenerator.h"
+#include "CallBranch.h"
 #include "FuncBranch.h"
 #include "VDEFBranch.h"
+#include "AssignBranch.h"
+#include "MathEBranch.h"
 
-CodeGenerator::CodeGenerator(Compiler* compiler) : CompilerEntity(compiler)
+CodeGenerator::CodeGenerator(Compiler* compiler, std::string code_gen_desc) : CompilerEntity(compiler)
 {
     this->stream = new Stream();
-    this->current_index = -1;
+    this->current_index = 0;
+    this->code_gen_desc = code_gen_desc;
 }
 
 CodeGenerator::~CodeGenerator()
@@ -42,73 +46,72 @@ CodeGenerator::~CodeGenerator()
     delete this->stream;
 }
 
-void CodeGenerator::startRegistrationProcess()
+void CodeGenerator::registerFunction(std::string func_name, std::vector<std::shared_ptr<Branch>> func_arguments, int func_mem_pos)
 {
-    if (this->current_index != -1)
-        throw CodeGeneratorException("Registration process has already started");
-
-    this->current_index = this->stream->getSize();
+    struct function func;
+    func.func_name = func_name;
+    for (std::shared_ptr<Branch> func_argument : func_arguments)
+    {
+        std::shared_ptr<VDEFBranch> vdef_func_argument = std::dynamic_pointer_cast<VDEFBranch>(func_argument);
+        struct function_argument argument;
+        argument.type = vdef_func_argument->getDefinitionTypeBranch()->getValue();
+        argument.value = vdef_func_argument->getDefinitionNameBranch()->getValue();
+        func.func_arguments.push_back(argument);
+    }
+    func.memory_pos = func_mem_pos;
+    functions.push_back(func);
 }
 
-void CodeGenerator::registerMemoryLocation(std::shared_ptr<Branch> branch)
+void CodeGenerator::registerFunctionCall(std::string func_name, std::vector<std::shared_ptr < Branch>> func_arguments)
 {
-    if (branch == NULL)
+    struct function_call func_call;
+    struct function_argument func_call_argument;
+    func_call.func_name = func_name;
+    for (std::shared_ptr<Branch> func_argument_branch : func_arguments)
     {
-        throw CodeGeneratorException("Attempting to register a memory location for a NULL branch..");
-    }
-
-    if (branch->getType() == "FUNC")
-    {
-        if (isFunctionRegistered(branch->getValue()))
+        std::string value = func_argument_branch->getValue();
+        std::string type = func_argument_branch->getType();
+        if (type == "identifier")
         {
-            throw CodeGeneratorException("Attempting to register an already registered branch");
+            std::shared_ptr<struct scope_variable> scope_var = this->getScopeVariable(value);
+            func_call_argument.type = scope_var->type;
+            func_call_argument.value = value;
         }
-    }
-    else
-    {
-        throw CodeGeneratorException("Cannot register branch type: " + branch->getType() + " as it is unknown how to register this particular branch");
-    }
+        else if (type == "number")
+        {
+            int value_num = std::atoi(value.c_str());
+            std::string type = this->getCompiler()->getTypeFromNumber(value_num);
+            func_call_argument.type = type;
+            func_call_argument.value = value;
+        }
 
-    if (this->current_index == -1)
-        throw CodeGeneratorException("You must invoke the startRegistrationProcess() method first");
-
-    struct location loc;
-    loc.branch = branch;
-    loc.location = this->current_index;
-    this->locations.push_back(loc);
-    this->current_index = -1;
+        func_call.func_arguments.push_back(func_call_argument);
+    }
+    func_call.memory_pos = this->stream->getPosition();
+    this->function_calls.push_back(func_call);
 }
 
 bool CodeGenerator::isFunctionRegistered(std::string func_name)
 {
-    for (struct location loc : this->locations)
+    for (int i = 0; i < this->functions.size(); i++)
     {
-        std::shared_ptr<Branch> branch = loc.branch;
-        if (branch->getType() == "FUNC")
+        struct function* func = &this->functions[i];
+        if (func->func_name == func_name)
         {
-            std::shared_ptr<FuncBranch> func_branch = std::dynamic_pointer_cast<FuncBranch>(branch);
-            if (func_branch->getFunctionNameBranch()->getValue() == func_name)
-            {
-                return true;
-            }
+            return true;
         }
     }
-
     return false;
 }
 
-int CodeGenerator::getFunctionMemoryLocation(std::string func_name)
+int CodeGenerator::getFunctionIndex(std::string func_name)
 {
-    for (struct location loc : this->locations)
+    for (int i = 0; i < this->functions.size(); i++)
     {
-        std::shared_ptr<Branch> branch = loc.branch;
-        if (branch->getType() == "FUNC")
+        struct function* func = &this->functions[i];
+        if (func->func_name == func_name)
         {
-            std::shared_ptr<FuncBranch> func_branch = std::dynamic_pointer_cast<FuncBranch>(branch);
-            if (func_branch->getFunctionNameBranch()->getValue() == func_name)
-            {
-                return loc.location;
-            }
+            return i;
         }
     }
     return -1;
@@ -120,6 +123,166 @@ void CodeGenerator::generate(std::shared_ptr<Tree> tree)
     for (std::shared_ptr<Branch> branch : root->getChildren())
     {
         this->generateFromBranch(branch);
+    }
+
+    // Now its time to populate the goblin object file
+    struct goblin_obj_header* gob_head = &this->gob_obj.header;
+    struct goblin_functions_declaration_header* func_declarations_header = &gob_head->func_declarations_header;
+    struct goblin_function_calls_header* func_calls_header = &gob_head->func_calls_header;
+
+    size_t stream_size = this->stream->getSize();
+    gob_head->code_generator_descriptor = this->code_gen_desc;
+
+    int total_func_decl = this->functions.size();
+    func_declarations_header->t_functions = total_func_decl;
+    func_declarations_header->functions = new goblin_function_declaration[total_func_decl];
+    if (total_func_decl > 0)
+    {
+        for (int i = 0; i < total_func_decl; i++)
+        {
+            struct function* function = &this->functions[i];
+            int total_func_args = function->func_arguments.size();
+            struct goblin_function_declaration* gob_func_decl = &func_declarations_header->functions[i];
+            gob_func_decl->func_name = function->func_name;
+            gob_func_decl->t_arguments = total_func_args;
+            if (total_func_args > 0)
+            {
+                gob_func_decl->arguments = new goblin_argument[total_func_args];
+            }
+            for (int a = 0; a < total_func_args; a++)
+            {
+                struct function_argument* argument = &function->func_arguments[a];
+                struct goblin_argument* gob_argument = &gob_func_decl->arguments[a];
+                gob_argument->type = argument->type;
+            }
+
+            gob_func_decl->mem_pos = function->memory_pos;
+        }
+    }
+    int total_func_calls = this->function_calls.size();
+    func_calls_header->total_func_calls = total_func_calls;
+    if (total_func_calls > 0)
+    {
+        func_calls_header->func_calls = new goblin_function_call[total_func_calls];
+    }
+
+    for (int i = 0; i < total_func_calls; i++)
+    {
+        struct function_call* func_call = &this->function_calls[i];
+        struct goblin_function_call* gob_func_call = &func_calls_header->func_calls[i];
+        int total_arguments = func_call->func_arguments.size();
+        gob_func_call->func_name = func_call->func_name;
+        gob_func_call->t_arguments = func_call->func_arguments.size();
+        if (total_arguments > 0)
+        {
+            gob_func_call->arguments = new goblin_argument[total_arguments];
+        }
+
+        for (int b = 0; b < total_arguments; b++)
+        {
+            struct function_argument* argument = &func_call->func_arguments[b];
+            struct goblin_argument* gob_argument = &gob_func_call->arguments[b];
+            gob_argument->type = argument->type;
+        }
+    }
+
+    gob_head->code_size = stream_size;
+    Stream* code_stream = &gob_head->code;
+    for (int i = 0; i < stream_size; i++)
+    {
+        code_stream->write8(this->stream->read8());
+    }
+
+    // Erase the stream as the code is now written in the code stream
+    this->stream->empty();
+
+    // Write the data to the goblin object stream
+    gob_obj.write();
+
+    // Store the goblin object in the stream
+    Stream* gob_obj_stream = gob_obj.getStream();
+    while (!gob_obj_stream->isEmpty())
+    {
+        this->stream->write8(gob_obj_stream->read8());
+    }
+
+}
+
+void CodeGenerator::generateFromBranch(std::shared_ptr<Branch> branch)
+{
+    if (branch->getType() == "FUNC")
+    {
+        std::shared_ptr<FuncBranch> funcBranch = std::dynamic_pointer_cast<FuncBranch>(branch);
+        std::vector<std::shared_ptr < Branch>> func_argument_branches = funcBranch->getFunctionArgumentBranches();
+        std::shared_ptr<Branch> scopeBranch = funcBranch->getFunctionScopeBranches();
+        int func_mem_pos = this->stream->getPosition();
+        this->scope_start(branch);
+        // Generate the scope
+        // Register the function arguments as a scope variable
+        for (std::shared_ptr<Branch> branch : func_argument_branches)
+        {
+            this->registerScopeVariable(branch);
+        }
+        this->handleScope(scopeBranch);
+        this->scope_end(branch);
+
+        this->clearScopeVariables();
+        this->registerFunction(funcBranch->getFunctionNameBranch()->getValue(), func_argument_branches, func_mem_pos);
+    }
+}
+
+void CodeGenerator::handleScope(std::shared_ptr<Branch> branch)
+{
+    if (branch->getType() == "V_DEF")
+    {
+        this->registerScopeVariable(branch);
+    }
+    else if (branch->getType() == "ASSIGN")
+    {
+        std::shared_ptr<AssignBranch> assign_branch = std::dynamic_pointer_cast<AssignBranch>(branch);
+        std::string var_name = branch->getChildren()[0]->getChildren()[0]->getValue();
+        std::shared_ptr<struct scope_variable> var = this->getScopeVariable(var_name);
+        this->scope_assign_start(branch, var);
+        // Handle the expression
+        std::vector<std::shared_ptr < Branch>> e_branches = this->getCompiler()->getASTAssistant()->findAllChildrenOfType(assign_branch->getValueBranch(), "MATH_E");
+        for (std::shared_ptr<Branch> e_branch : e_branches)
+        {
+            std::shared_ptr<MathEBranch> math_e_branch = std::dynamic_pointer_cast<MathEBranch>(e_branch);
+            this->scope_handle_exp(math_e_branch);
+        }
+        this->scope_assign_end(branch, var);
+    }
+    else if (branch->getType() == "CALL")
+    {
+        std::shared_ptr<CallBranch> callBranch = std::dynamic_pointer_cast<CallBranch>(branch);
+        std::string name = callBranch->getFunctionNameBranch()->getValue();
+        std::vector<std::shared_ptr < Branch>> arguments = callBranch->getFunctionArgumentBranches();
+        for (std::shared_ptr<Branch> argument : arguments)
+        {
+            if (argument->getType() == "number")
+            {
+                this->scope_handle_number(argument);
+            }
+            else if (argument->getType() == "MATH_E")
+            {
+                std::vector<std::shared_ptr < Branch>> e_branches = this->getCompiler()->getASTAssistant()->findAllChildrenOfType(argument, "MATH_E");
+                for (std::shared_ptr<Branch> e_branch : e_branches)
+                {
+                    std::shared_ptr<MathEBranch> math_e_branch = std::dynamic_pointer_cast<MathEBranch>(e_branch);
+                    this->scope_handle_exp(math_e_branch);
+                }
+
+            }
+        }
+
+        this->registerFunctionCall(name, arguments);
+        this->scope_func_call(branch, name, arguments);
+    }
+
+    std::vector<std::shared_ptr < Branch>> children = branch->getChildren();
+    for (std::shared_ptr<Branch> child : children)
+    {
+        handleScope(child);
     }
 }
 
@@ -156,13 +319,23 @@ std::shared_ptr<struct scope_variable> CodeGenerator::getScopeVariable(std::stri
         if (variable->name == name)
             return variable;
     }
-    
+
     return NULL;
 }
 
 void CodeGenerator::clearScopeVariables()
 {
     this->scope_variables.clear();
+}
+
+GoblinObject* CodeGenerator::getGoblinObject()
+{
+    return &this->gob_obj;
+}
+
+std::string CodeGenerator::getCodeGeneratorDescriptor()
+{
+    return this->code_gen_desc;
 }
 
 Stream* CodeGenerator::getStream()
