c0ba7723e6b5fc371902cf24b5fca2b9eb38dcd7
Razor <daniel@xeno.solutions>
2016-07-28 01:37:01 +0100

    Added a branches.h file to contain includes for all the branches, this means that only the branches.h file has to be included, for those who need access to all branch types.

---
+++ include/branches.h
@@ -0,0 +1,40 @@
+/*
+    Goblin compiler v1.0 - The standard compiler for the Goblin language.
+    Copyright (C) 2016  Daniel McCarthy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * File:   branches.h
+ * Author: dansb
+ *
+ * Created on 26 July 2016, 00:52
+ */
+
+#ifndef BRANCHES_H
+#define BRANCHES_H
+
+
+#include "CallBranch.h"
+#include "FuncBranch.h"
+#include "VDEFBranch.h"
+#include "AssignBranch.h"
+#include "MathEBranch.h"
+#include "EBranch.h"
+#include "ASMBranch.h"
+#include "ArrayBranch.h"
+
+
+#endif /* BRANCHES_H */
+
