720afcd2cb4756431eae995413b699699c8bf31e
Razor <daniel@xeno.solutions>
2017-04-03 01:53:03 +0100

    Implemented the "xchg reg8, reg8" and the "xchg reg16, reg16" instructions.

--- codegens/8086CodeGen/src/Assembler8086.cpp
+++ codegens/8086CodeGen/src/Assembler8086.cpp
@@ -80,7 +80,7 @@ unsigned char ins_map[] = {
     0xc1, 0xcd, 0x38, 0x39, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d,
     0x80, 0x81, 0x80, 0x81, 0x8d, 0xd2, 0xd3, 0xd2, 0xd3, 0xf6,
     0xf7, 0xf6, 0xf7, 0x84, 0x85, 0x84, 0x85, 0x84, 0x85, 0xa8,
-    0xa9, 0xf6, 0xf7
+    0xa9, 0xf6, 0xf7, 0x86, 0x87
 };
 
 // instruction size excluding OOMMM and OORRRMMM rules that change the size (you should still include the OOMMM and OORRRMMM byte)
@@ -98,7 +98,7 @@ unsigned char ins_sizes[] = {
     3, 2, 2, 2, 2, 2, 2, 2, 2, 3,
     3, 4, 3, 4, 2, 2, 2, 2, 2, 2,
     2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    3, 3, 4
+    3, 3, 4, 2, 2
 };
 
 
@@ -118,7 +118,7 @@ unsigned char static_rrr[] = {
     3, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     7, 7, 7, 7, 0, 3, 3, 2, 2, 5,
     5, 7, 7, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0
+    0, 0, 0, 0, 0
 };
 
 /* Describes information relating to an instruction 
@@ -261,6 +261,8 @@ INSTRUCTION_INFO ins_info[] = {
     USE_W | HAS_REG_USE_LEFT | HAS_IMM_USE_RIGHT, // test ax, imm16
     HAS_OOMMM | HAS_REG_USE_LEFT | HAS_IMM_USE_RIGHT, // test reg8, imm8
     USE_W | HAS_OOMMM |HAS_REG_USE_LEFT | HAS_IMM_USE_RIGHT, // test reg16, imm16
+    HAS_OORRRMMM | HAS_REG_USE_LEFT | HAS_REG_USE_RIGHT, // xchg reg8, reg8
+    USE_W | HAS_OORRRMMM | HAS_REG_USE_LEFT | HAS_REG_USE_RIGHT, // xchg reg16, reg16
 };
 
 struct ins_syntax_def ins_syntax[] = {
@@ -396,7 +398,9 @@ struct ins_syntax_def ins_syntax[] = {
     "test", TEST_ACC_WITH_IMM_W0, AL_IMM8,
     "test", TEST_ACC_WITH_IMM_W1, AX_IMM16,
     "test", TEST_REG_WITH_IMM_W0, REG8_IMM8,
-    "test", TEST_REG_WITH_IMM_W1, REG16_IMM16
+    "test", TEST_REG_WITH_IMM_W1, REG16_IMM16,
+    "xchg", XCHG_REG_WITH_REG_W0, REG8_REG8,
+    "xchg", XCHG_REG_WITH_REG_W1, REG16_REG16
 };
 
 /* Certain instructions have condition codes that specify a particular event.
@@ -478,6 +482,7 @@ Assembler8086::Assembler8086(Compiler* compiler, std::shared_ptr<VirtualObjectFo
     Assembler::addInstruction("rcr");
     Assembler::addInstruction("cmp");
     Assembler::addInstruction("test");
+    Assembler::addInstruction("xchg");
 
     this->left = NULL;
     this->right = NULL;
